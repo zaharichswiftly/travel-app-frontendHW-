fd35778ca35f2cf87aab1edaee5c4705af735d54
zaharich09 <97597336+zaharichswiftly@users.noreply.github.com>
2022-11-13 21:09:22 +0300

    footer

---
+++ app/components/common/footer/Footer.jsx
@@ -0,0 +1,44 @@
+import styles from './Footer.module.scss'
+import { useRouter } from 'next/router'
+
+const navItems = [
+    {
+        icon: 'home',
+        link: '/',
+    },
+    {
+        icon: 'explore',
+        link: '/explore',
+    },
+    {
+        icon: 'place',
+        link: '/place',
+    },
+    {
+        icon: 'person_outline',
+        link: '/profile',
+    },
+]
+
+const Footer = () => {
+    const { push, pathname } = useRouter()
+
+    return (
+    <footer className={styles.footer}>
+        <nav>
+            {navItems.map(item =>(
+            <button 
+            className={pathname === item.link ? styles.active : ''}
+            onClick={() => push(item.link)}
+            key={item.icon}
+            >
+                <span className='material-icons-outlined'>{item.icon}</span>
+
+            </button>
+            ))}
+        </nav>
+    </footer>
+    )
+}
+
+export default Footer;
