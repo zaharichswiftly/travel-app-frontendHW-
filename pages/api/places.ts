27d8fe2916435dd492c8dfa8e63dae14d187faf0
zaharich09 <97597336+zaharichswiftly@users.noreply.github.com>
2022-11-14 21:21:41 +0300

    Places cards

--- pages/api/places.ts
+++ pages/api/places.ts
@@ -1,22 +1,31 @@
-import { IPlace } from "./../../app/types/place"
-import japanImg from "../../assets/images/japan.jpg"
-import { NextApiRequest, NextApiResponse } from "next"
-
+import { NextApiRequest, NextApiResponse } from 'next'
+import { IPlace } from '../../app/types/place'
 
 const places: IPlace[] = [
-    {
-        slug: 'tokyo',
-        description: 'lorem ipsum',
-        imagePath: japanImg.src,
-        location: 'Japan',
-        rating: 5,
-        duration: '10 days',
-        distance: 10453,
-        googleMapLink: 'wefedf',
-        mapImage: 'wgfw',
-    },
+	{
+		slug: 'tokyo',
+		description: 'wefwewegewwefew',
+		imagePath: 'http://localhost:3000/images/places/japan.jpg',
+		location: { country: 'Japan', city: 'Kyoto' },
+		rating: 5,
+		duration: '10 days',
+		distance: 10453,
+		googleMapLink: 'wefewf',
+		mapImage: 'egfw',
+	},
+	{
+		slug: 'venezia',
+		description: 'wefwewegewwefew',
+		imagePath: 'http://localhost:3000/images/places/italy.jpg',
+		location: { country: 'Italy', city: 'Venezia' },
+		rating: 5,
+		duration: '7 days',
+		distance: 9453,
+		googleMapLink: 'wefewf',
+		mapImage: 'egfw',
+	},
 ]
 
-export default function hadler(req: NextApiRequest, res: NextApiResponse) {
-  res.status(200).json(places)
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+	res.status(200).json(places)
 }
