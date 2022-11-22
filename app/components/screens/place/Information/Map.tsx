3b55c6c0415e8999545cb4118274061aa8e28896
Timur Byazrov <trbyazrov@avito.ru>
2022-11-22 16:31:37 +0300

    Ну фикс какой-то

--- app/components/screens/place/Information/Map.tsx
+++ app/components/screens/place/Information/Map.tsx
@@ -1,24 +1,31 @@
-import React from "react"
-import { ComposableMap, Geographies, Geography, ZoomableGroup } from "react-simple-maps"
+import React from "react";
+import {
+  ComposableMap,
+  Geographies,
+  Geography,
+  ZoomableGroup,
+} from "react-simple-maps";
 
 const geoUrl =
-  "https://raw.githubusercontent.com/deldersveld/topojson/master/world-countries.json"
+  "https://raw.githubusercontent.com/deldersveld/topojson/master/world-countries.json";
 
 export default function MapChart() {
   return (
     <ComposableMap>
-	<ZoomableGroup>
-      <Geographies geography={geoUrl}>
-        {({ geographies }) =>
-          geographies.map((geo) => (
-			  <Geography key={geo.rsmKey} geography={geo}
-			  fill="#807e7f"
-          	  stroke="#e8e8e8" 
-			  />
-			  ))
-			}
-      </Geographies>
-	  </ZoomableGroup>
+      <ZoomableGroup>
+        <Geographies geography={geoUrl}>
+          {({ geographies }) =>
+            geographies.map((geo) => (
+              <Geography
+                key={geo.rsmKey}
+                geography={geo}
+                fill="#807e7f"
+                stroke="#e8e8e8"
+              />
+            ))
+          }
+        </Geographies>
+      </ZoomableGroup>
     </ComposableMap>
-  )
+  );
 }
